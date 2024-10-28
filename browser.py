5d42a95cdef42dcfcd04ab467b859eede76e7cb0
WJ-Yuan <59187817+WJ-Yuan@users.noreply.github.com>
2024-10-28 11:29:08 +0800

    :sparkles: different text size

--- browser.py
+++ browser.py
@@ -21,8 +21,10 @@ class Layout:
     self.cursor_x, self.cursor_y = HSTEP, VSTEP
     self.weight, self.style = "normal", "roman"
 
+    self.line = []
     for tok in tokens:
       self.token(tok)
+    self.flush()
 
   def token(self, tok: Tag):
     if isinstance(tok, Text):
@@ -44,6 +46,11 @@ class Layout:
       self.weight = "bold"
     elif tok.tag == "/b":
       self.weight = "normal"
+    elif tok.tag == 'br':
+      self.flush()
+    elif tok.tag == '/p':
+      self.flush()
+      self.cursor_y += VSTEP
 
   def word(self, word):
     font = tkfont.Font(
@@ -52,12 +59,28 @@ class Layout:
           slant=self.style
         )
     w = font.measure(word)
-    self.display_list.append((self.cursor_x, self.cursor_y, word, font))
-    self.cursor_x += w + font.measure(" ")
 
     if self.cursor_x + w >= WIDTH - HSTEP:
-      self.cursor_y += font.metrics("linespace") * 1.25
-      self.cursor_x = HSTEP
+      self.flush()
+    self.line.append((self.cursor_x, word, font))
+    self.cursor_x += w + font.measure(" ")
+
+  def flush(self):
+    if not self.line:
+      return
+    metrics = [font.metrics() for x, word, font in self.line]
+    max_ascent = max([metric["ascent"] for metric in metrics])
+    baseline = self.cursor_y + 1.25 * max_ascent
+
+    for x, word, font in self.line:
+      y = baseline - font.metrics("ascent")
+      self.display_list.append((x, y, word, font))
+
+    max_descent = max([metric["descent"] for metric in metrics])
+    self.cursor_y = baseline + 1.25 * max_descent
+    self.cursor_x = HSTEP
+    self.line = []
+
 
 
 def lex(body):
@@ -106,7 +129,7 @@ class Browser:
     for x, y, c, f in self.display_list:
       # 跳过屏幕外的字符绘制
       if y > self.scroll + HEIGHT: continue
-      if y + VSTEP < self.scroll: continue
+      if y + f.metrics("linespace") < self.scroll: continue
 
       self.canvas.create_text(x, y - self.scroll, text=c, font=f, anchor="nw")
 
