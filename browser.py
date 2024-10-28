a50cbc6346da150e644e104b5e6d255e279683fb
WJ-Yuan <59187817+WJ-Yuan@users.noreply.github.com>
2024-10-28 10:57:54 +0800

    :sparkles: text font

--- browser.py
+++ browser.py
@@ -4,36 +4,84 @@ from url import URL
 WIDTH, HEIGHT = 800, 600
 HSTEP, VSTEP = 13, 18
 SCROLL_STEP = 100
+FONT_SIZE = 12
 
-def layout(text):
-  font = tkfont.Font()
-  display_text = []
-  cursor_x, cursor_y = HSTEP, VSTEP
+class Tag:
+  def __init__(self, tag):
+    self.tag = tag
 
-  for word in text.split():
+class Text:
+  def __init__(self, text):
+    self.text = text
+
+class Layout:
+  def __init__(self, tokens):
+    self.display_list = []
+    self.size = FONT_SIZE
+    self.cursor_x, self.cursor_y = HSTEP, VSTEP
+    self.weight, self.style = "normal", "roman"
+
+    for tok in tokens:
+      self.token(tok)
+
+  def token(self, tok: Tag):
+    if isinstance(tok, Text):
+      for word in tok.text.split():
+        self.word(word)
+    elif tok.tag == "small":
+        self.size -= 2
+    elif tok.tag == "/small":
+        self.size += 2
+    elif tok.tag == "big":
+        self.size += 4
+    elif tok.tag == "/big":
+        self.size -= 4
+    elif tok.tag == "i":
+      self.style = "italic"
+    elif tok.tag == "/i":
+      self.style = "roman"
+    elif tok.tag == "b":
+      self.weight = "bold"
+    elif tok.tag == "/b":
+      self.weight = "normal"
+
+  def word(self, word):
+    font = tkfont.Font(
+          size=self.size,
+          weight=self.weight,
+          slant=self.style
+        )
     w = font.measure(word)
-    display_text.append((cursor_x, cursor_y, word))
-    cursor_x += w + font.measure(" ")
+    self.display_list.append((self.cursor_x, self.cursor_y, word, font))
+    self.cursor_x += w + font.measure(" ")
 
-    if cursor_x + w >= WIDTH - HSTEP:
-      cursor_y += font.metrics("linespace") * 1.25
-      cursor_x = HSTEP
+    if self.cursor_x + w >= WIDTH - HSTEP:
+      self.cursor_y += font.metrics("linespace") * 1.25
+      self.cursor_x = HSTEP
 
-  return display_text
 
 def lex(body):
+  out = []
+  buffer = ""
   in_tag = False
-  text = ""
 
   for c in body:
     if c == "<":
       in_tag = True
+      if buffer:
+        out.append(Text(buffer))
+      buffer = ""
     elif c == ">":
       in_tag = False
-    elif not in_tag:
-      text += c
+      out.append(Tag(buffer))
+      buffer = ""
+    else:
+      buffer += c
 
-  return text
+  if not in_tag and buffer:
+    out.append(Text(buffer))
+
+  return out
 
 class Browser:
   def __init__(self):
@@ -55,17 +103,17 @@ class Browser:
 
   def draw(self):
     self.canvas.delete("all")
-    for x, y, c, in self.display_text:
+    for x, y, c, f in self.display_list:
       # 跳过屏幕外的字符绘制
       if y > self.scroll + HEIGHT: continue
       if y + VSTEP < self.scroll: continue
 
-      self.canvas.create_text(x, y - self.scroll, text=c, anchor="nw")
+      self.canvas.create_text(x, y - self.scroll, text=c, font=f, anchor="nw")
 
-  def load(self, url):
+  def load(self, url: URL):
     body = url.request()
-    text = lex(body)
-    self.display_text = layout(text)
+    tokens = lex(body)
+    self.display_list = Layout(tokens).display_list
     self.draw()
 
 
